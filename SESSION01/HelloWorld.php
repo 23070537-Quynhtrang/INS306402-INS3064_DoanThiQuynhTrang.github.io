2f1a441817636141ffd3d8b3aa701fa0ebb45471
23070537-Quynhtrang <23070537@vnu.edu.vn>
2026-01-20 15:50:55 +0700

    Create HelloWorld.php

---
+++ SESSION01/HelloWorld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+// Exercise 1: Hello World
+// Author: Quynh Trang
+
+echo "Hello, I am Quynh Trang!";
+echo "<br>";
+echo "Welcome to INS3064!";
+?>
+
+</body>
+</html>
